e15570557e1c07b55ca766d2a5a33cb2d8fce600
antond <anton.danilov@brainsphere.de>
2024-12-31 19:43:04 +0100

    add pin for can transceiver

--- LinBridge.ino
+++ LinBridge.ino
@@ -12,8 +12,8 @@
 #endif
 
 void setup() {
-    Serial.begin(115200);              // Debugging output
-    Serial1.begin(19200, SERIAL_8N1);  // LIN communication at 19,200 baud
+    Serial.begin(115200);
+    Serial1.begin(19200, SERIAL_8N1, GPIO_NUM_3, GPIO_NUM_2);
     LOG("LIN Master Initialized");
 }
 
